73e7edeab1713f230fbe92bf5094603fc210982a
CyprianDavis <128586128+CyprianDavis@users.noreply.github.com>
2024-01-20 01:22:39 +0300

    add queue inteface and class

---
+++ src/data/structure/ArrayQueue.java
@@ -0,0 +1,10 @@
+package data.structure;
+/**
+ *Implementation of Queue ADT using array
+ * @author CYPRIAN DAVIS
+ *
+ */
+
+public class ArrayQueue<T> {
+
+}
---
+++ src/data/structure/Queue.java
@@ -0,0 +1,5 @@
+package data.structure;
+
+public interface Queue<T> {
+
+}
